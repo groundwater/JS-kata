0b59f2817cb49e12e2e28896957033da98663924
Jacob Groundwater <groundwater@gmail.com>
2016-05-02 20:26:04 -0700

    Create 02-call.js

---
+++ forms/02-call.js
@@ -0,0 +1,16 @@
+function double(x) {
+  return x+x
+}
+
+// what is the type and value of
+double(2)
+double('hi')
+double
+double(double('hi'))
+
+// which of these will work
+
+double(double(23))
+double(double)
+double(double)()
+double(23)(double)
