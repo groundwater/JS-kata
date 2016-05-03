f11042687e979a7f8209660cc6f9c711c9e48852
Jacob Groundwater <groundwater@gmail.com>
2016-05-02 20:20:41 -0700

    Update and rename 1.js to 01-define.js

---
+++ forms/01-define.js
@@ -0,0 +1,12 @@
+// define the following functions
+
+// take a number, return add 1
+
+// take a string, append an explaination mark
+
+// return today's date
+
+// return if a number is odd or even
+
+// return how many letters the string has 
+
