b164c84a26ac2016c4733c89f9af3a8d6dee5cd7
Jacob Groundwater <groundwater@gmail.com>
2016-05-02 20:30:58 -0700

    Create 03-params.js

---
+++ forms/03-params.js
@@ -0,0 +1,13 @@
+// write a function that 
+
+// adds two numbers (x,y)
+
+// repeats string num number of times (str, num)
+
+// difference of two numbers
+
+// larger of two numbers
+
+// multiply two numbers
+
+// 
