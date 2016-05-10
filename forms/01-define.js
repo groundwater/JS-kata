b747b4f63f76d89005bc14d3f7cad2028826f6f6
Sofia Nguy <sofianguy@gmail.com>
2016-05-09 22:47:58 -0700

    define functions. add functions to define

--- forms/01-define.js
+++ forms/01-define.js
@@ -1,12 +1,44 @@
 // define the following functions
 
 // take a number, return add 1
+function addOne(num) {
+	return num + 1
+}
 
-// take a string, append an explaination mark
+// take a string, append an exclamation mark
+function strExclamation(str) {
+	return str + '!'
+}
 
 // return today's date
+function todayDate() {
+	return new Date()
+}
 
 // return if a number is odd or even
+function numberOddEven(num) {
+	if (num%2 === 0) {
+		return 'even'
+	} else if (num%2 === 1) {
+		return 'odd'
+	} else {
+		return 'idk'
+	}
+}
 
 // return how many letters the string has 
+function strLetters(str) {
+	return str.length
+}
+
+// take a number, return times 3
+
+
+// take a number, return half
+
+
+// take a string, return first letter
+
+
+// take a string, return last letter
 
